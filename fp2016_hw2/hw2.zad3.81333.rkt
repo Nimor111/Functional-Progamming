76755788143f718819262509d0c010f6597d46f8
Nimor111 <georgi.bojinov@hotmail.com>
2016-11-26 13:07:49 +0200

    Adding zad3

--- fp2016_hw2/hw2.zad3.81333.rkt
+++ fp2016_hw2/hw2.zad3.81333.rkt
@@ -1,13 +1,19 @@
 #lang racket
 
-(define ((max-abs-lst lst) x)
+(define (max-abs-lst lst x)
   (if (= 1 (length lst))
       (abs ((first lst) x))
-      (max (abs ((first lst) x)) ((max-abs-lst (rest lst)) x))))
+      (max (abs ((first lst) x)) (max-abs-lst (rest lst) x))))
+
+(define (return-func-val func-lst x)
+  (if (equal? (abs ((first func-lst) x)) (max-abs-lst func-lst x))
+      ((first func-lst) x)
+      (return-func-val (rest func-lst) x)))
 
 (define (maximize func-lst)
-  (max-abs-lst func-lst))
+  (lambda (x) (return-func-val func-lst x)))
 
-((maximize (list (λ (x) (- x 10)) (λ (x) (- x 5)))) 5) ;-> 5
+((maximize (list (λ (x) (- x 10)))) 5)
+((maximize (list (λ (x) (- x 10)) (λ (x) (- x 5)))) 5) ;-> -5
 ((maximize (list (λ (x) (- x 10)) (λ (x) (- x 5)))) 9) ;-> 4
 ((maximize (list (λ (x) (expt x 2)) (λ (x) (expt x 3)) (λ (x) (expt x 4)))) 2)
