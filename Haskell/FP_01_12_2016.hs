0e00a3a8f95c93ad419cebe4ba2db06214c0b5f8
Nimor111 <georgi.bojinov@hotmail.com>
2016-12-04 15:49:48 +0200

    Some syntax and lang standard fixes in first haskell file

--- Haskell/FP_01_12_2016.hs
+++ Haskell/FP_01_12_2016.hs
@@ -21,19 +21,19 @@ digits n = helper n 0
   where
     helper :: Integer -> Integer -> Integer
     helper n sum = if n < 10
-        then (sum + n)
+        then sum + n
         else helper (n `div` 10) (sum + n `mod` 10)
 
-num_digs :: Integer -> Integer
-num_digs 0 = 0
-num_digs n = 1 + num_digs (n `div` 10)
+numDigs :: Integer -> Integer
+numDigs 0 = 0
+numDigs n = 1 + num_digs (n `div` 10)
 
 reverse' :: Integer -> Integer
 reverse' n = helper n 0 (num_digs n - 1)
   where
     helper :: Integer -> Integer -> Integer -> Integer
     helper n new pow = if n < 10
-        then (new + n * 10 ^ pow)
+        then new + n * 10 ^ pow
         else helper (n `div` 10) (new + (n `mod` 10) * 10 ^ pow) (pow - 1)
 
 prime :: Integer -> Bool
@@ -42,32 +42,31 @@ prime n = helper 2
   where
    helper :: Integer -> Bool
    helper k
-      | (fromIntegral k) > (fromIntegral n ** 0.5) = True
+      | fromIntegral k > sqrt (fromIntegral n) = True
       | n `mod` k == 0 = False
       | otherwise = helper (k + 1)
 
-prime_interval :: Integer -> Integer -> Integer
-prime_interval a b = helper a 0
+primeInterval :: Integer -> Integer -> Integer
+primeInterval a b = helper a 0
   where
     helper :: Integer -> Integer -> Integer
     helper a sum
         | a > b = 0
-        | prime a == True = a + helper (a + 1) (sum + a)
-        | prime a == False = helper (a + 1) sum
+        | prime a = a + helper (a + 1) (sum + a)
+        | not (prime a) = helper (a + 1) sum
 
 sumElems :: [Int] -> Int
 sumElems xs  =
-  if (null xs)
+  if null xs
     then 0
-    else (head xs) + sumElems (tail xs)
+    else head xs + sumElems (tail xs)
 
 sumElems2 :: [Int] -> Int
-sumElems2 [] = 0
-sumElems2 (x:xs) = x + sumElems2 xs
+sumElems2 = foldr sum  0 xs
 
-remove_elem :: Int -> [Int] -> [Int]
-remove_elem _ [] = []
-remove_elem e (x:xs) =
-  if (e == x)
+removeElem :: Int -> [Int] -> [Int]
+removeElem _ [] = []
+removeElem e (x:xs) =
+  if e == x
     then xs
-    else x:(remove_elem e xs)
+    else x:removeElem e xs
