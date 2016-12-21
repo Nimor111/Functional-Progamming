c5f18cbcc95b9fb0b96dbfe6f9316f09fae9a243
Nimor111 <georgi.bojinov@hotmail.com>
2016-12-21 10:01:13 +0200

    Add insertion sort to dabblings

--- Haskell/haskell_dabbling.hs
+++ Haskell/haskell_dabbling.hs
@@ -1,3 +1,8 @@
- inc x = x + pesho
-	where
-		pesho = 1
+iSort :: [Int] -> [Int]
+iSort = foldr ins []
+
+ins :: Int -> [Int] -> [Int]
+ins x [] = [x]
+ins x (y:ys)
+  | x <= y = x:(y:ys)
+  | otherwise = y : ins x ys
