dc29cbb59bada317fb7865b7cad53f2e1da0930b
Georgi Bojinov <georgi.bojinov@hotmail.com>
2016-12-22 11:07:21 +0200

    Add files via upload

---
+++ FP_22_12_2016.hs
@@ -0,0 +1,42 @@
+import Data.List
+import Data.Char
+import Data.List.Split
+
+main :: IO()
+main = do
+ -- print (grouping (mapNums("a123b2c56")))
+ print(sumNumbers("a123b2c56"))
+ print(matrixSum [[1,2,3], [4,5,6]] [[4,5,6], [1,2,3]])
+ print(transposeMatrix [[1,2], [4,5]])
+ --print(zipMult [1,2,3] [4,5,6])
+ 
+sumNumbers :: String -> Int
+sumNumbers xs = sum (map (\ x -> fromDigits x) (splitWhen (> 10) (mapNums "a123b2c56")))
+
+fromDigits :: [Int] -> Int
+fromDigits = foldl addDigit 0
+  where addDigit num d = 10 * num + d
+
+mapNums :: String -> [Int]
+mapNums xs = map (\ x -> ord x - 48 ) xs
+
+matrixSum :: [[Int]] -> [[Int]] -> [[Int]]
+matrixSum xxs [] = xxs
+matrixSum [] xxs = xxs
+matrixSum (x:xxs) (y:yys) = zipWith (+) x y : matrixSum xxs yys
+
+transposeMatrix :: [[Int]] -> [[Int]]
+transposeMatrix ([]:_) = []
+transposeMatrix xxs = (map head xxs) : (transposeMatrix (map tail xxs))
+
+--multMatrix 
+
+--zipMult :: [[a]] -> [[a]]
+--zipMult (x:xxs) = zip x (zipMult xss)
+
+
+
+
+
+
+
