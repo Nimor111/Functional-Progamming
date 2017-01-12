bd776928f5ea8a101f639ff1c846017f0c3e8e19
Nimor111 <georgi.bojinov@hotmail.com>
2017-01-12 18:32:09 +0200

    Add desc of graph representation, fix an error

--- Haskell/Homeworks/FP_HW4/hw4.81333.task3.hs
+++ Haskell/Homeworks/FP_HW4/hw4.81333.task3.hs
@@ -1,3 +1,9 @@
+-- Избраното представяне на графа е списък на ребрата.
+-- Структурата се състои от списък от наредени двойки, 
+-- всяка от които съдържа в първата си част единия край на реброто,
+-- а във втората - другия край. Присъствие на една наредена двойка в списъка
+-- означава, че тези два върха са свързани.
+
 type Vertex = Int
 type Edge = (Vertex, Vertex)
 type Graph = [Edge]
@@ -9,21 +15,22 @@ getSuccessors (e:g) v
   | otherwise = getSuccessors g v
 
 allPaths :: Vertex -> Graph -> [[Vertex]]
-allPaths v g = ways
+allPaths v g = paths 
     where
       vertexEdges = filter (\f -> fst f == v) g
       succs = getSuccessors g v
-      ways = if null vertexEdges
-             then [[v]]
-             else map (\x -> v : x) $ concatMap (`allPaths` g) succs
+      paths = if null vertexEdges
+              then [[v]]
+              else map (\x -> v : x) $ concatMap (`allPaths` g) succs
 
 isSumReachable :: Graph -> Vertex -> Int -> Bool
 isSumReachable g v s =
-  any (`findSumInList` s) (allPaths v g)
+  any (`findSumInList` s) (filter (\x -> length x /= 1) (allPaths v g))
 
 findSumInList :: [Int] -> Int -> Bool
 findSumInList xs s = helper xs 0
   where
+    helper :: [Int] -> Int -> Bool
     helper (x:xs) sum
       | sum == s = True
       | null xs && (sum + x) == s = True
