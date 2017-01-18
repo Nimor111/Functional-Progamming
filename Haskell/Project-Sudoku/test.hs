1d8627f574d6c402e934238da02ab4f7f833a9e3
Nimor111 <georgi.bojinov@hotmail.com>
2017-01-18 17:06:03 +0200

    Modify tests

--- Haskell/Project-Sudoku/test.hs
+++ Haskell/Project-Sudoku/test.hs
@@ -1,7 +1,7 @@
 import Sudoku
 import SudokuChecker
 
-easyBoard = [[0,0,0,2,6,0,7,0,1],
+boardsOne = [[0,0,0,2,6,0,7,0,1],
              [6,8,0,0,7,0,0,9,0],
              [1,9,0,0,0,4,5,0,0],
              [8,2,0,1,0,0,0,4,0],
@@ -11,7 +11,7 @@ easyBoard = [[0,0,0,2,6,0,7,0,1],
              [0,4,0,0,5,0,0,3,6],
              [7,0,3,0,1,8,0,0,0]]
 
-mediumBoard = [[0,2,0,6,0,8,0,0,0],
+boardsTwoos = [[0,2,0,6,0,8,0,0,0],
                [5,8,0,0,0,9,7,0,0],
                [0,0,0,0,4,0,0,0,0],
                [3,7,0,0,0,0,5,0,0],
@@ -22,27 +22,13 @@ mediumBoard = [[0,2,0,6,0,8,0,0,0],
                [0,0,0,3,0,6,0,9,0]]
 
 
-hardBoard = [[0,0,0,6,0,0,4,0,0],
-             [7,0,0,0,0,3,6,0,0],
-             [0,0,0,0,9,1,0,8,0],
-             [0,0,0,0,0,0,0,0,0],
-             [0,5,0,1,8,0,0,0,3],
-             [0,0,0,3,0,6,0,4,5],
-             [0,4,0,2,0,0,0,6,0],
-             [9,0,3,0,0,0,0,0,0],
-             [0,2,0,0,0,0,1,0,0]]
-
 main = do
-  putStrLn "Easy board: "
-  printBoard easySolved
-  print $ checkSudoku easySolved
-  putStrLn "\nMedium board: "
-  printBoard mediumSolved
-  print $ checkSudoku mediumSolved
-  putStrLn "\nHard board: "
-  printBoard hardSolved
-  print $ checkSudoku hardSolved
-    where
-      easySolved = solveSudoku easyBoard
-      mediumSolved = solveSudoku mediumBoard
-      hardSolved = solveSudoku hardBoard
+  putStrLn "First board: "
+  printBoard firstBoard 
+  print $ checkSudoku firstBoard 
+  putStrLn "\nSecond board: "
+  printBoard secondBoard 
+  print $ checkSudoku secondBoard 
+   where
+      firstBoard = solveSudoku boardsOne
+      secondBoard = solveSudoku boardsTwoos
