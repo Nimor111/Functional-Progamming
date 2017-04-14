8c04a3034ebd007755695791861078c42532658f
Nimor111 <georgi.bojinov@hotmail.com>
2017-04-15 01:03:28 +0300

    Add progress on 99 haskell problems, back to haskell

---
+++ Haskell/Personal/99-Haskell-Problems/ElevenToTwenty.hs
@@ -0,0 +1,55 @@
+import OneToTen
+import Control.Applicative
+
+data Operation a = Multiple Int a | Single a deriving Show
+
+
+-- Problem 11
+encodeModified :: Eq a => [a] -> [Operation a]
+encodeModified xs = map (\x -> if length x == 1
+                              then Single (head x)
+                              else Multiple (length x) (head x))
+                        (pack xs)
+
+-- Problem 12
+decodeModified :: Eq a => [Operation a] -> [a]
+decodeModified [] = []
+decodeModified (Multiple x y : xs) = replicate x y ++ decodeModified xs
+decodeModified (Single x : xs) = x : decodeModified xs
+
+decodeModified' :: Eq a => [Operation a] -> [a]
+decodeModified' = foldr (\x acc -> helper x ++ acc) []
+  where
+    helper :: Eq a => Operation a -> [a]
+    helper (Multiple x y) = replicate x y
+    helper (Single x) = [x]
+
+-- Problem 13
+encodeDirect :: Eq a => [a] -> [Operation a]
+encodeDirect [] = []
+encodeDirect [x] = [Single x]
+encodeDirect (x:xs)
+  | x == head xs = Multiple (numberOfMultiples x xs + 1) x : encodeDirect (drop (numberOfMultiples x xs) xs)
+  | x /= head xs = Single x : encodeDirect xs
+  where
+    numberOfMultiples :: Eq a => a -> [a] -> Int 
+    numberOfMultiples _ [] = 0
+    numberOfMultiples y (x:xs)
+      | y == x = 1 + numberOfMultiples y xs
+      | y /= x = 0
+
+-- Problem 14
+dupli :: [a] -> [a]
+dupli = foldr (\x acc -> x : x : acc) []
+
+dupli' :: [a] -> [a]
+dupli' [] = []
+dupli' (x:xs) = x : x : dupli' xs
+
+-- list monad wohoo
+dupli'' :: [a] -> [a]
+dupli'' xs = xs >>= (\x -> [x, x])
+
+-- applicative functor
+dupli''' :: [a] -> [a]
+dupli''' = (<**> [id, id])
--- Haskell/Personal/99-Haskell-Problems/OneToTen.hs
+++ Haskell/Personal/99-Haskell-Problems/OneToTen.hs
@@ -1,8 +1,18 @@
+module OneToTen 
+  (pack,
+   encode)
+  where
+-- for &&&
+
+import Control.Arrow
+
+-- Problem 1
 myLast :: [a] -> a
 myLast [] = error "Empty list!"
 myLast [x] = x
 myLast (x:xs) = myLast xs
 
+-- Problem 2
 myButLast :: [a] -> a
 myButLast [] = error "Empty list!"
 myButLast [x] = error "One element!"
@@ -10,21 +20,26 @@ myButLast [x, y] = x
 myButLast (x:xs) = myButLast xs
 
 -- index from 1
+-- Problem 3
 elementAt :: [a] -> Int -> a
 elementAt [] _ = error "Empty list!"
 elementAt (x:xs) 1 = x
 elementAt (x:xs) y = elementAt xs (y - 1)
 
+-- Problem 4
 myLength :: [a] -> Int
 myLength = foldr (\ x -> (+) 1) 0
 
+-- Problem 5
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
 
+-- Problem 6
 isPalindrome :: Eq a => [a] -> Bool
 isPalindrome xs = myReverse xs == xs
 
+-- Problem 7
 data NestedList a = Elem a | List [NestedList a] deriving Show
 
 getElemValue :: NestedList a -> a
@@ -40,4 +55,23 @@ flatten (List []) = []
 flatten (Elem i) = [i]
 flatten (List (x:xs)) = flatten x ++ flatten (List xs)
 
--- flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+-- Problem 8
+compress :: Eq a => [a] -> [a]
+compress [] = []
+compress (x:xs) = x : compress (dropWhile (== x) xs)
+
+compress' :: Eq a => [a] -> [a]
+compress' xs = foldr (\x acc -> if x == head acc then acc else x : acc) [last xs] xs
+
+-- Problem 9
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = (x : takeWhile (== x) xs) : (pack . dropWhile (== x)) xs
+
+-- Problem 10
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = map (length &&& head) (pack xs)
+
+-- function that, given an input n, returns "n+1" and "n*2", uses Arrows
+-- calc1 :: Int -> (Int,Int)
+-- calc1 = (+1) &&& (*2)
