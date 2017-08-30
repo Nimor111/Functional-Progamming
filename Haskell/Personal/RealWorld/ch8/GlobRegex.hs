d7d3a03b137b282e02eec3978909f96e0e17a645
Nimor111 <georgi.bojinov@hotmail.com>
2017-08-31 02:16:34 +0300

    Refactor error handling with Either TODO refactor namesMatching

--- Haskell/Personal/RealWorld/ch8/GlobRegex.hs
+++ Haskell/Personal/RealWorld/ch8/GlobRegex.hs
@@ -1,36 +1,119 @@
 module GlobRegex
   (
-    globToRegex,
-    matchesGlob
+    matchesGlob,
+    -- namesMatching
   ) where
 
 import Text.Regex.Posix ((=~))
 import Data.Char (toLower)
+import System.Directory (doesDirectoryExist, doesFileExist,
+                         getCurrentDirectory, getDirectoryContents)
+import System.FilePath ((</>), isPathSeparator, dropTrailingPathSeparator, splitFileName)
+import Control.Monad (forM)
+import Control.Exception (handle)
 
-matchesGlob :: FilePath -> String -> Bool -> Bool
+type GlobError = String
+
+-- how many files and dirs match glob in a directory
+-- namesMatching :: String -> IO [FilePath]
+-- namesMatching pat
+--   | not ( isPattern pat ) = do
+--     exists <- doesNameExist pat
+--     return [pat | exists]
+--   | otherwise = do
+--     case splitFileName pat of 
+--       ("./", baseName) -> do
+--         curDir <- getCurrentDirectory
+--         listMatches curDir baseName
+--       (dirName, baseName) -> do
+--         dirs <- if isPattern dirName
+--                then namesMatching (dropTrailingPathSeparator dirName)
+--                else return [dirName]
+--         let listDir = if isPattern baseName
+--                       then listMatches
+--                       else listPlain
+--         pathNames <- forM dirs $ \dir -> do
+--                         baseNames <- listDir dir baseName
+--                         return (map (dir </>) baseNames)
+--         return (concat pathNames)
+
+-- doesNameExist :: FilePath -> IO Bool
+-- doesNameExist name = do
+--   fileExists <- doesFileExist name
+--   return (fileExists || doesDirectoryExist name)
+
+-- listMatches :: FilePath -> String -> IO [FilePath]
+-- listMatches dirName pat = do
+--   dirName' <- if null dirName
+--              then getCurrentDirectory
+--              else return dirName
+--   handle (const (return [])) $ do
+--     names <- getDirectoryContents dirName'
+--     let names' = if isHidden pat
+--                  then filter isHidden names
+--                  else filter ( not . isHidden ) names
+--     return (filter (match' pat) names')
+--     where
+--       match' :: String -> FilePath -> Bool
+--       match' pat cs = case isPathSeparator '/' of
+--                         True -> matchesGlob cs pat True
+--                         False -> matchesGlob cs pat False
+
+-- isHidden :: String -> Bool
+-- isHidden ('.':_) = True
+-- isHidden _       = False
+
+-- listPlain :: FilePath -> String -> IO [FilePath]
+-- listPlain dirName baseName = do
+--   exists <- if null baseName
+--            then doesDirectoryExist dirName
+--            else doesNameExist (dirName </> baseName)
+--   return (if exists then [baseName] else [])
+
+-- -- check if string contains pattern characters, if not, simply check filename
+-- isPattern :: String -> Bool
+-- isPattern = any (`elem` "[*?")
+
+matchesGlob :: FilePath -> String -> Bool -> Either GlobError Bool
 matchesGlob name pat sensitive 
-  | sensitive = name =~ globToRegex' pat
-  | otherwise = lowercase name =~ (lowercase . globToRegex') pat
+  | sensitive = case globToRegex' pat of 
+                  Left err -> Left err
+                  Right result -> Right (name =~ result)
+  | otherwise = case globToRegex' pat of
+                  Left err -> Left err
+                  Right result -> Right (lowercase name =~ lowercase result)
   where 
     lowercase :: String -> String
     lowercase = map toLower
                                           
 -- he anchor
-globToRegex :: String -> String
-globToRegex cs = "^" ++ globToRegex' cs ++ "$"
+globToRegex :: String -> Either GlobError String
+globToRegex cs = case globToRegex' cs of
+                   Left err -> Left err
+                   Right result -> Right ("^" ++ result ++ "$")
 
 -- he match
-globToRegex' :: String -> String
-globToRegex' "" = ""
+globToRegex' :: String -> Either GlobError String
+globToRegex' "" = Right ""
 
-globToRegex' ('*':cs) = ".*" ++ globToRegex' cs 
-globToRegex' ('?':cs) = "." ++ globToRegex' cs 
-globToRegex' ('[':'!':c:cs) = "[^" ++ c : charClass cs 
+globToRegex' ('*':cs) = case globToRegex' cs of
+                          Left err -> Left err
+                          Right result -> Right (".*" ++ result)
+globToRegex' ('?':cs) = case globToRegex' cs of
+                          Left err -> Left err
+                          Right result -> Right ("." ++ result)
+globToRegex' ('[':'!':c:cs) = case charClass cs of
+                                Left err -> Left err
+                                Right result -> Right ("[^" ++ c : result)
 
-globToRegex' ('[':c:cs) = '[' : c : charClass cs 
-globToRegex' ('[':_) = error "unterminated character class"
+globToRegex' ('[':c:cs) = case charClass cs of
+                            Left err -> Left err
+                            Right result -> Right ('[' : c : result)
+globToRegex' ('[':_) = Left "unterminated character class"
 
-globToRegex' (c:cs) = escape c ++ globToRegex' cs
+globToRegex' (c:cs) = case globToRegex' cs of
+                        Left err -> Left err
+                        Right result -> Right (escape c ++ result)
 
 -- but most importantly
 -- he escap
@@ -41,7 +124,11 @@ escape c
   where
     regexChars = "\\+()^$.{}]|"
 
-charClass :: String -> String
-charClass (']':cs) = ']' : globToRegex' cs 
-charClass (c:cs) = c : charClass cs 
-charClass [] = error "unterminated character class"
+charClass :: String -> Either GlobError String
+charClass (']':cs) = case globToRegex' cs of
+                       Left err -> Left err
+                       Right result -> Right (']' : result)
+charClass (c:cs) = case charClass cs of
+                     Left err -> Left err
+                     Right result -> Right (c : result)
+charClass [] = Left "unterminated character class"
