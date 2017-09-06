a0a4c5b5de08096c4a0d934ceb47021e8f1de951
Nimor111 <georgi.bojinov@hotmail.com>
2017-09-07 01:24:06 +0300

    Add encode func for vigenere cipher

--- Haskell/Personal/Dabblings/VigenereCipher.hs
+++ Haskell/Personal/Dabblings/VigenereCipher.hs
@@ -1,6 +1,32 @@
 module VigenereCipher (
   vEncode,
-  vDecode
+  -- vDecode
 ) where
 
+import           Data.Char (chr, ord)
 
+generateKeywordString :: String -> String -> String
+generateKeywordString source target = helper source target target
+  where
+    helper :: String -> String -> String -> String
+    helper [] _ _ = []
+    helper (s:ss) target@(t:ts) ot
+      | s == ' ' = ' ' : helper ss target ot
+      | null ts = t : helper ss ot ot
+      | otherwise = t : helper ss ts ot
+
+getAlphabetKey :: Char -> Int
+getAlphabetKey c = ord c - 65
+
+getCorrespondingLetter :: Char -> Char -> Char
+getCorrespondingLetter ' ' ' ' = ' '
+getCorrespondingLetter sc tc
+  | value > 90 = chr $ value - 26
+  | otherwise = chr value
+  where
+    value = ord sc + getAlphabetKey tc
+
+vEncode :: String -> String -> String
+vEncode source key = foldr (\(x,y) acc -> getCorrespondingLetter x y : acc) [] letters
+  where
+    letters = zip source (generateKeywordString source key)
